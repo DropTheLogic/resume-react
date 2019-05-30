e0896c84bee90604bab70b486882f1dc22d5ce0c
Daniel Marquez <DannyBrianMarquez@gmail.com>
2019-05-30 12:39:07 -0400

    style: Reformat resume data spacing and quotations

--- src/resume_data.js
+++ src/resume_data.js
@@ -1,108 +1,108 @@
 export const navItems = ['Profile', 'Resume', 'Portfolio'];
 
 export const bioData = {
-	name : {name: 'Danny Márquez', faclass: 'fas fa-user'},
-	phone : {name: '(917) 399-5264', faclass: 'fas fa-phone'},
-	email : {name: 'DannyBrianMarquez@gmail.com', url: 'mailto:DannyBrianMarquez@gmail.com', faclass: 'fas fa-envelope'},
-	website : {name : 'danny-marquez.com', url : 'https://www.danny-marquez.com', faclass: 'fas fa-home'},
-	github: {name : 'DropTheLogic', url : 'https://github.com/DropTheLogic', faclass: 'fab fa-github'},
-	codepen : {name : 'DropTheLogic', url : 'https://www.codepen.io/DropTheLogic', faclass: 'fab fa-codepen'},
-	linkedin : {name : 'dannymarquez', url : 'https://www.linkedin.com/in/dannymarquez/', faclass: 'fab fa-linkedin'},
-	twitter: {name : '@DropTheLogic', url : 'https://www.twitter.com/DropTheLogic', faclass: 'fab fa-twitter'}
+	name: {name: 'Danny Márquez', faclass: 'fas fa-user'},
+	phone: {name: '(917) 399-5264', faclass: 'fas fa-phone'},
+	email: {name: 'DannyBrianMarquez@gmail.com', url: 'mailto:DannyBrianMarquez@gmail.com', faclass: 'fas fa-envelope'},
+	website: {name: 'danny-marquez.com', url: 'https://www.danny-marquez.com', faclass: 'fas fa-home'},
+	github: {name: 'DropTheLogic', url: 'https://github.com/DropTheLogic', faclass: 'fab fa-github'},
+	codepen: {name: 'DropTheLogic', url: 'https://www.codepen.io/DropTheLogic', faclass: 'fab fa-codepen'},
+	linkedin: {name: 'dannymarquez', url: 'https://www.linkedin.com/in/dannymarquez/', faclass: 'fab fa-linkedin'},
+	twitter: {name: '@DropTheLogic', url: 'https://www.twitter.com/DropTheLogic', faclass: 'fab fa-twitter'}
 };
 
 export const resumeData = {
-	"Jobs" : [
+	Jobs: [
 		{
-				"title" : "Web Developer/Graphic Designer",
-				"employer" : "Freelance",
-				"dates" : "2001-current",
-				"location" : "615 W 173 St, New York City",
-				"description" : [
-					"Design and maintain websites and/or promotional material for artists and musicians",
-					"Work with clients to find theme and feel for their visual representation",
-					"Develop and maintain web sites for clients, using modern responsive design"
-					],
-				"url" : "http://www.danny-marquez.com/portfolio/"
-			},
-			{
-				"title" : "Manager",
-				"employer" : "Nick's Family-Style Restaurant and Pizzeria",
-				"dates" : "2003-current",
-				"location" : "1814 2nd Ave, New York City",
-				"description" : [
-					"Oversee and manage mid-sized restaurant and staff of over 20 workers",
-					"Responsible for inventory and ordering of all restaurant dry-goods, food and alcohol",
-					"Coordinate, staff and retain crews of prep workers, cooks and servers",
-					"Create brand loyalty and provide customer service for strong repeat business"
-					],
-				"url" : "http://www.nicksnyc.com"
-			},
-			{
-				"title" : "Art Director",
-				"employer" : "Foul Magazine",
-				"dates" : "2001-2002",
-				"location" : "12 St Marks Place, New York City",
-				"description" : [
-					"Designed, laid-out and prepared all elements for a video-game/lifestyle print magazine",
-					"Coordinated with staff to prepare design, art, editorial and advertising for print",
-					"Worked under tight deadlines to push issues out on-time",
-					"Delegated and managed art and design to subordinates as needed"
-					],
-				"url" : "http://www.villagevoice.com/best-of/2008/arts-and-entertainment/best-video-game-magazine-6445513"
-			}
-	],
-	"University Study" : [
-			{
-				"name" : "New York University",
-				"dates" : "1997-2001",
-				"location" : "1 Washington Place, New York City",
-				"department" : "Gallatin School of Individualized Study",
-				"degree" : "Bachelor of Arts",
-				"majors" : ["Concentration: Technology in the Arts"],
-				"url" : "http://gallatin.nyu.edu/"
-			}
-	],
-	"Online Courses" : [
+			title: 'Web Developer/Graphic Designer',
+			employer: 'Freelance',
+			dates: '2001-current',
+			location: '615 W 173 St, New York City',
+			description: [
+				'Design and maintain websites and/or promotional material for artists and musicians',
+				'Work with clients to find theme and feel for their visual representation',
+				'Develop and maintain web sites for clients, using modern responsive design'
+				],
+			url: 'http://www.danny-marquez.com/portfolio/'
+		},
 		{
-			"title" : "Front-End Web Developer Nanodegree",
-			"date" : "2015-2016",
-			"school" : "Udacity",
-			"url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+			title: 'Manager',
+			employer: "Nick's Family-Style Restaurant and Pizzeria",
+			dates: '2003-current',
+			location: '1814 2nd Ave, New York City',
+			description: [
+				'Oversee and manage mid-sized restaurant and staff of over 20 workers',
+				'Responsible for inventory and ordering of all restaurant dry-goods, food and alcohol',
+				'Coordinate, staff and retain crews of prep workers, cooks and servers',
+				'Create brand loyalty and provide customer service for strong repeat business'
+				],
+			url: 'http://www.nicksnyc.com'
+		},
+		{
+			title: 'Art Director',
+			employer: 'Foul Magazine',
+			dates: '2001-2002',
+			location: '12 St Marks Place, New York City',
+			description: [
+				'Designed, laid-out and prepared all elements for a video-game/lifestyle print magazine',
+				'Coordinated with staff to prepare design, art, editorial and advertising for print',
+				'Worked under tight deadlines to push issues out on-time',
+				'Delegated and managed art and design to subordinates as needed'
+				],
+			url: 'http://www.villagevoice.com/best-of/2008/arts-and-entertainment/best-video-game-magazine-6445513'
 		}
 	],
-	"Skills" : ['HTML & CSS', 'Responsive Web Design', 'Javascript', 'jQuery', 'Git/GitHub', 'React.js', 'Knockout.js', 'Google Maps API', 'Bootstrap', 'Wordpress', 'Graphic Design', 'Adobe Photoshop', 'Adobe Illustrator', 'Mac / OSX / iOS', 'PC / Windows', 'Management']
+	'University Study': [
+		{
+			name: 'New York University',
+			dates: '1997-2001',
+			location: '1 Washington Place, New York City',
+			department: 'Gallatin School of Individualized Study',
+			degree: 'Bachelor of Arts',
+			majors: ['Concentration: Technology in the Arts'],
+			url: 'http://gallatin.nyu.edu/'
+		}
+	],
+	'Online Courses': [
+		{
+			title: 'Front-End Web Developer Nanodegree',
+			date: '2015-2016',
+			school: 'Udacity',
+			url: 'https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001'
+		}
+	],
+	Skills: ['HTML & CSS', 'Responsive Web Design', 'Javascript', 'jQuery', 'Git/GitHub', 'React.js', 'Knockout.js', 'Google Maps API', 'Bootstrap', 'Wordpress', 'Graphic Design', 'Adobe Photoshop', 'Adobe Illustrator', 'Mac / OSX / iOS', 'PC / Windows', 'Management']
 };
 
 export const portfolioData = [
 	{
-		"title" : "The Geber Girls",
-		"caption" : "Single page band website using vanilla Javascript, HTML5 and CSS.",
-		"img" : "./images/geber-screen-opt.png",
-		"url" : "https://www.gebergirls.com"
+		title: 'The Geber Girls',
+		caption: 'Single page band website using vanilla Javascript, HTML5 and CSS.',
+		img: './images/geber-screen-opt.png',
+		url: 'https://www.gebergirls.com'
 	},
 	{
-		"title" : "Time Calculator (beta)",
-		"caption" : "Web app to aid in time-based arithmetic calculations. Built with React.js.",
-		"img" : "./images/danny-marquez-time-calculator-opt.png",
-		"url" : "https://codepen.io/DropTheLogic/full/WEzRPZ"
+		title: 'Time Calculator (beta)',
+		caption: 'Web app to aid in time-based arithmetic calculations. Built with React.js.',
+		img: './images/danny-marquez-time-calculator-opt.png',
+		url: 'https://codepen.io/DropTheLogic/full/WEzRPZ'
 	},
 	{
-		"title" : "Schedulizer (alpha)",
-		"caption" : "Spreadsheet inspired web app for scheduling employee work shifts. Built using Javascript and Knockout.js two-way binding.",
-		"img" : "./images/schedulizer-screenshot-opt.png",
-		"url" : "https://github.com/DropTheLogic/schedulizer"
+		title: 'Schedulizer (alpha)',
+		caption: 'Spreadsheet inspired web app for scheduling employee work shifts. Built using Javascript and Knockout.js two-way binding.',
+		img: './images/schedulizer-screenshot-opt.png',
+		url: 'https://github.com/DropTheLogic/schedulizer'
 	},
 	{
-		"title" : "The Williamsburger",
-		"caption" : "Responsive web mapping app using Google Maps and Foursquare APIs to map a curated list of burger restaurants in Williamsburg.",
-		"img" : "./images/danny-marquez-williamsburger-screen.png",
-		"url" : "https://www.danny-marquez.com/williamsburger/"
+		title: 'The Williamsburger',
+		caption: 'Responsive web mapping app using Google Maps and Foursquare APIs to map a curated list of burger restaurants in Williamsburg.',
+		img: './images/danny-marquez-williamsburger-screen.png',
+		url: 'https://www.danny-marquez.com/williamsburger/'
 	},
 	{
-		"title" : "Bug Run! - OOP Javascript Game",
-		"caption" : "Simple arcade game made using Javascript and HTML5 canvas.",
-		"img" : "./images/danny-marquez-game-screen.png",
-		"url" : "https://danny-marquez.com/BugRun/"
+		title: 'Bug Run! - OOP Javascript Game',
+		caption: 'Simple arcade game made using Javascript and HTML5 canvas.',
+		img: './images/danny-marquez-game-screen.png',
+		url: 'https://danny-marquez.com/BugRun/'
 	},
 ];
